3e8612c795802e8c7582b0cedd9da2f26815ab00
Jolien Trog <jolien.trog@icinga.com>
2024-03-19 13:29:14 +0100

    Implement ItemController with indexAction

---
+++ lib/Controller/ItemController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Nachtmerrie\Controller;
+
+use Nachtmerrie\Database\Item;
+use Nachtmerrie\Database\Table;
+use Nachtmerrie\Select;
+use Nachtmerrie\View;
+
+
+class ItemController extends Controller
+{
+    public function indexAction()
+    {
+        $select = (new Select($this->connection))
+            ->columns(['id', 'nl', 'de'])
+            ->from(new Item());
+
+        $result = $select->fetchAll();
+
+
+        $viewObject = (new View())
+            ->setOuterLayout('outer-layout.phtml')
+            ->setInnerLayout('index.phtml')
+            ->setTitle('Nachtmerrie')
+            ->setData('WL','Woordenlijst')
+            ->setData('result', $result)
+            ->setStylesheet('index.css');
+
+        echo $viewObject->render();
+    }
+
+}
+
